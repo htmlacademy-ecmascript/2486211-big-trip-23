ee5d4a21fa44827ab8c8e32b0e2a4fd96faefdca
yourskynas <nastiazaharova69@gmail.com>
2024-04-28 20:53:47 +0300

    Правит названия переменных

--- src/main.js
+++ src/main.js
@@ -4,12 +4,12 @@ import EventsListPresenter from './presenter/events-list-presenter.js';
 import SortingView from './view/sorting-view.js';
 import { render, RenderPosition } from './render.js';
 
-const tripMainEl = document.querySelector('.trip-main');
-const tripFiltersEl = tripMainEl.querySelector('.trip-controls__filters');
-const tripEventsSectionEl = document.querySelector('.trip-events');
-const eventsListPresenter = new EventsListPresenter({eventsListContainer: tripEventsSectionEl});
+const tripMainElement = document.querySelector('.trip-main');
+const tripFiltersElement = tripMainElement.querySelector('.trip-controls__filters');
+const tripEventsSectionElement = document.querySelector('.trip-events');
+const eventsListPresenter = new EventsListPresenter({eventsListContainer: tripEventsSectionElement});
 
-render(new TripInfoView(), tripMainEl, RenderPosition.AFTERBEGIN);
-render(new FiltersView(), tripFiltersEl);
-render(new SortingView(), tripEventsSectionEl);
+render(new TripInfoView(), tripMainElement, RenderPosition.AFTERBEGIN);
+render(new FiltersView(), tripFiltersElement);
+render(new SortingView(), tripEventsSectionElement);
 eventsListPresenter.init();
