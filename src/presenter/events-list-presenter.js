5e7233a81e999dc4c80592f2d68ee901ebb479aa
yourskynas <nastiazaharova69@gmail.com>
2024-05-02 20:42:49 +0300

    Подключит модель, временно скроет отрисовку view для создания новой точки

--- src/presenter/events-list-presenter.js
+++ src/presenter/events-list-presenter.js
@@ -1,24 +1,27 @@
 import EventsListView from '../view/events-list.js';
-import EventsItemView from '../view/events-point.js';
+import EventsPointView from '../view/events-point.js';
 import EditorPointView from '../view/editor-point.js';
-import NewPointView from '../view/new-point.js';
+// import NewPointView from '../view/new-point.js';
 import { render } from '../render.js';
 
 
 export default class EventsListPresenter {
   eventsListComponent = new EventsListView();
 
-  constructor({eventsListContainer}) {
+  constructor({eventsListContainer, pointsModel}) {
     this.eventsListContainer = eventsListContainer;
+    this.pointsModel = pointsModel;
   }
 
   init() {
-    render(this.eventsListComponent, this.eventsListContainer);
-    render(new EditorPointView(), this.eventsListComponent.getElement());
-    render(new NewPointView(), this.eventsListComponent.getElement());
+    this.eventsListPoints = [...this.pointsModel.getPoints()];
 
-    for (let i = 0; i < 3; i++) {
-      render(new EventsItemView(), this.eventsListComponent.getElement());
+    render(this.eventsListComponent, this.eventsListContainer);
+    render(new EditorPointView({point: this.eventsListPoints[0]}), this.eventsListComponent.getElement());
+    // render(new NewPointView(), this.eventsListComponent.getElement());
+
+    for (let i = 1; i < this.eventsListPoints.length; i++) {
+      render(new EventsPointView({point: this.eventsListPoints[i]}), this.eventsListComponent.getElement());
     }
   }
 }
