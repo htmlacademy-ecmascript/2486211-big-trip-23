0bff376f4f12faa0daa6f1315f4cf3d7851eb6cb
yourskynas <nastiazaharova69@gmail.com>
2024-05-10 09:10:43 +0300

    Делает часть полей в классах приватными

--- src/model/points-model.js
+++ src/model/points-model.js
@@ -5,20 +5,20 @@ import { mockOffers } from '../mock/offer.js';
 const POINT_COUNT = 4;
 
 export default class PointsModel {
-  points = Array.from({length: POINT_COUNT}, getRandomPoint);
-  destinations = mockDestinations;
-  offers = mockOffers;
+  #points = Array.from({length: POINT_COUNT}, getRandomPoint);
+  #destinations = mockDestinations;
+  #offers = mockOffers;
 
-  getPoints() {
-    return this.points;
+  get points() {
+    return this.#points;
   }
 
   getDestinations() {
-    return this.destinations;
+    return this.#destinations;
   }
 
   getOffers() {
-    return this.offers;
+    return this.#offers;
   }
 
   getDestinationsById(id) {
--- src/presenter/events-list-presenter.js
+++ src/presenter/events-list-presenter.js
@@ -5,32 +5,37 @@ import { render } from '../framework/render.js';
 
 
 export default class EventsListPresenter {
-  eventsListComponent = new EventsList();
+  #eventsListContainer = null;
+  #pointsModel = null;
+
+  #eventsListComponent = new EventsList();
+
+  #eventsListPoints = [];
 
   constructor({eventsListContainer, pointsModel}) {
-    this.eventsListContainer = eventsListContainer;
-    this.pointsModel = pointsModel;
+    this.#eventsListContainer = eventsListContainer;
+    this.#pointsModel = pointsModel;
   }
 
   init() {
-    this.eventsListPoints = [...this.pointsModel.getPoints()];
+    this.#eventsListPoints = [...this.#pointsModel.points];
 
-    render(this.eventsListComponent, this.eventsListContainer);
+    render(this.#eventsListComponent, this.#eventsListContainer);
     const editorPoint = new EditorPoint({
-      point: this.eventsListPoints[0],
-      allOffers: this.pointsModel.getOffersByType(this.eventsListPoints[0].type),
-      pointDestination: this.pointsModel.getDestinationsById(this.eventsListPoints[0].destination),
-      allDestination: this.pointsModel.getDestinations()
+      point: this.#eventsListPoints[0],
+      allOffers: this.#pointsModel.getOffersByType(this.#eventsListPoints[0].type),
+      pointDestination: this.#pointsModel.getDestinationsById(this.#eventsListPoints[0].destination),
+      allDestination: this.#pointsModel.getDestinations()
     });
-    render(editorPoint, this.eventsListComponent.element);
+    render(editorPoint, this.#eventsListComponent.element);
 
-    for (let i = 1; i < this.eventsListPoints.length; i++) {
+    for (let i = 1; i < this.#eventsListPoints.length; i++) {
       const point = new EventsPoint({
-        point: this.eventsListPoints[i],
-        offers: [...this.pointsModel.getOffersById(this.eventsListPoints[i].type, this.eventsListPoints[i].offers)],
-        destination: this.pointsModel.getDestinationsById(this.eventsListPoints[i].destination)
+        point: this.#eventsListPoints[i],
+        offers: [...this.#pointsModel.getOffersById(this.#eventsListPoints[i].type, this.#eventsListPoints[i].offers)],
+        destination: this.#pointsModel.getDestinationsById(this.#eventsListPoints[i].destination)
       });
-      render(point, this.eventsListComponent.element);
+      render(point, this.#eventsListComponent.element);
     }
   }
 }
