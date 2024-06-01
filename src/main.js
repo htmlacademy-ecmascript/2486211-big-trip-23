3a31860c6f57aead9e5535cff3a563f3bdaf04ca
yourskynas <nastiazaharova69@gmail.com>
2024-06-02 02:59:34 +0300

    Добавляет модель для фильтра, рефакторит фильтрацию

--- src/main.js
+++ src/main.js
@@ -1,21 +1,31 @@
+import PagePresenter from './presenter/page-presenter.js';
 import TripInfo from './view/trip-info.js';
 import Filters from './view/filters.js';
 import PointsModel from './model/points-model.js';
+import FilterModel from './model/filter-model.js';
 import { RenderPosition, render } from './framework/render.js';
-import { generateFilter } from './mock/filter.js';
-import PagePresenter from './presenter/page-presenter.js';
+
+const filters = [
+  {
+    type: 'Everything',
+    count: 0,
+  },
+];
 
 const tripMainElement = document.querySelector('.trip-main');
 const tripFiltersElement = tripMainElement.querySelector('.trip-controls__filters');
 const tripEventsSectionElement = document.querySelector('.trip-events');
 const pointsModel = new PointsModel();
+const filterModel = new FilterModel();
 const pagePresenter = new PagePresenter({
   eventsListContainer: tripEventsSectionElement,
   pointsModel,
 });
 
-const filters = generateFilter(pointsModel.points);
-
 render(new TripInfo(), tripMainElement, RenderPosition.AFTERBEGIN);
-render(new Filters({filters}), tripFiltersElement);
+render(new Filters({
+  filters,
+  currentFilterType: 'Everything',
+  onFilterTypeChange: () => {}
+}), tripFiltersElement);
 pagePresenter.init();
