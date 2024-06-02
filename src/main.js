f56235db55ea0569941f7e315874aa287d90304b
yourskynas <nastiazaharova69@gmail.com>
2024-06-03 01:34:42 +0300

    Добавит презентер фильтров

--- src/main.js
+++ src/main.js
@@ -1,31 +1,29 @@
 import PagePresenter from './presenter/page-presenter.js';
+import FilterPresenter from './presenter/filter-presenter.js';
 import TripInfo from './view/trip-info.js';
-import Filters from './view/filters.js';
 import PointsModel from './model/points-model.js';
 import FilterModel from './model/filter-model.js';
 import { RenderPosition, render } from './framework/render.js';
 
-const filters = [
-  {
-    type: 'Everything',
-    count: 0,
-  },
-];
-
 const tripMainElement = document.querySelector('.trip-main');
 const tripFiltersElement = tripMainElement.querySelector('.trip-controls__filters');
 const tripEventsSectionElement = document.querySelector('.trip-events');
+
 const pointsModel = new PointsModel();
 const filterModel = new FilterModel();
+
 const pagePresenter = new PagePresenter({
   eventsListContainer: tripEventsSectionElement,
   pointsModel,
+  filterModel
+});
+const filterPresenter = new FilterPresenter({
+  filterContainer: tripFiltersElement,
+  filterModel,
+  pointsModel
 });
 
 render(new TripInfo(), tripMainElement, RenderPosition.AFTERBEGIN);
-render(new Filters({
-  filters,
-  currentFilterType: 'Everything',
-  onFilterTypeChange: () => {}
-}), tripFiltersElement);
+
+filterPresenter.init();
 pagePresenter.init();
---
+++ src/presenter/filter-presenter.js
@@ -0,0 +1,62 @@
+import { UpdateType } from '../constants.js';
+import { remove, render, replace } from '../framework/render.js';
+import { filter } from '../utils/filter.js';
+import Filters from '../view/filters.js';
+
+export default class FilterPresenter {
+  #filterContainer = null;
+  #filterModel = null;
+  #pointsModel = null;
+
+  #filterComponent = null;
+
+  constructor({filterContainer, filterModel, pointsModel}) {
+    this.#filterContainer = filterContainer;
+    this.#filterModel = filterModel;
+    this.#pointsModel = pointsModel;
+
+    this.#pointsModel.addObserver(this.#handleModelEvent);
+    this.#filterModel.addObserver(this.#handleModelEvent);
+  }
+
+  get filters() {
+    const points = this.#pointsModel.points;
+
+    return Object.entries(filter).map(
+      ([filterType, filterPoints]) => ({
+        type: filterType,
+        count: filterPoints(points).length,
+      }));
+  }
+
+  init() {
+    const filters = this.filters;
+    const prevFilterComponent = this.#filterComponent;
+
+    this.#filterComponent = new Filters({
+      filters,
+      currentFilterType: this.#filterModel.filter,
+      onFilterTypeChange: this.#handleFilterTypeChange
+    });
+
+    if (prevFilterComponent === null) {
+      render(this.#filterComponent, this.#filterContainer);
+      return;
+    }
+
+    replace(this.#filterComponent, prevFilterComponent);
+    remove(prevFilterComponent);
+  }
+
+  #handleModelEvent = () => {
+    this.init();
+  };
+
+  #handleFilterTypeChange = (filterType) => {
+    if (this.#filterModel.filter === filterType) {
+      return;
+    }
+
+    this.#filterModel.setFilter(UpdateType.MAJOR, filterType);
+  };
+}
