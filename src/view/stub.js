bc685e32fe11cfc3c713b524b76ac5e7d80c053f
yourskynas <nastiazaharova69@gmail.com>
2024-06-03 01:37:31 +0300

    Изменяет компонент заглушку в зависимости от фильтра

--- src/view/stub.js
+++ src/view/stub.js
@@ -1,18 +1,23 @@
 import AbstractView from '../framework/view/abstract-view.js';
+import { StubText } from '../constants.js';
 
-const createStubTemplate = (text) => `
-    <p class="trip-events__msg">${text}</p>
-`;
+const createStubTemplate = (filterType) => {
+  const listEmptyValue = StubText[filterType];
+  return (`
+  <p class="trip-events__msg">${listEmptyValue}</p>
+`);
+};
 
 export default class Stub extends AbstractView {
-  #text = null;
-  constructor(text) {
+  #filterType = null;
+
+  constructor({filterType}) {
     super();
-    this.#text = text;
+    this.#filterType = filterType;
   }
 
   get template() {
-    return createStubTemplate(this.#text);
+    return createStubTemplate(this.#filterType);
   }
 }
 
