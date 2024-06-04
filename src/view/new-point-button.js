38b1e463db256bee76c26fd9442a4340a3337727
yourskynas <nastiazaharova69@gmail.com>
2024-06-05 00:15:16 +0300

    Создает презентер новой точки и компонент с кнопкой добавлениия точки

---
+++ src/view/new-point-button.js
@@ -0,0 +1,22 @@
+import AbstractView from '../framework/view/abstract-view.js';
+
+const createNewPointButtonTemplate = () => '<button class="trip-main__event-add-btn  btn  btn--big  btn--yellow" type="button">New event</button>';
+
+export default class NewPointButton extends AbstractView {
+  #handleClick = null;
+
+  constructor({onClick}) {
+    super();
+    this.#handleClick = onClick;
+    this.element.addEventListener('click', this.#clickHandler);
+  }
+
+  get template() {
+    return createNewPointButtonTemplate();
+  }
+
+  #clickHandler = (evt) => {
+    evt.preventDefault();
+    this.#handleClick();
+  };
+}
