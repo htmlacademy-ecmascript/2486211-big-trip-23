b1a7cdb9dcf3dc623bae56b6444aa082c29d2c1d
yourskynas <nastiazaharova69@gmail.com>
2024-06-07 23:11:26 +0300

    Поправляет логику фильтрации

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -4,7 +4,7 @@ import { isEventOver, isEventToday, isFutureEvent } from './date-format.js';
 const filter = {
   [FilterType.EVERYTHING]: (points) => points,
   [FilterType.FUTURE]: (points) => points.filter((point) => isFutureEvent(point.dateFrom)),
-  [FilterType.PAST]: (points) => points.filter((point) => isEventOver(point.dateFrom)),
+  [FilterType.PAST]: (points) => points.filter((point) => isEventOver(point.dateTo)),
   [FilterType.PRESENT]: (points) => points.filter((point) => isEventToday(point.dateFrom)),
 };
 
