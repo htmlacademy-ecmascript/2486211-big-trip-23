a82601d4aa6a3a7ec92a0684e7e5d942f2ae35c3
yourskynas <nastiazaharova69@gmail.com>
2024-06-11 01:37:20 +0300

    Добавит PointsApiService в main.js

--- src/main.js
+++ src/main.js
@@ -5,12 +5,18 @@ import PointsModel from './model/points-model.js';
 import FilterModel from './model/filter-model.js';
 import { RenderPosition, render } from './framework/render.js';
 import NewPointButton from './view/new-point-button.js';
+import PointsApiService from './server/points-api-service.js';
+
+const END_POINT = 'https://23.objects.htmlacademy.pro/big-trip';
+const AUTHORIZATION = 'Basic eksy6jld7dw';
 
 const tripMainElement = document.querySelector('.trip-main');
 const tripFiltersElement = tripMainElement.querySelector('.trip-controls__filters');
 const tripEventsSectionElement = document.querySelector('.trip-events');
 
-const pointsModel = new PointsModel();
+const pointsModel = new PointsModel({
+  pointsApiService: new PointsApiService(END_POINT, AUTHORIZATION)
+});
 const filterModel = new FilterModel();
 
 const pagePresenter = new PagePresenter({
